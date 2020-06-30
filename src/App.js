4a52378a490531d310389efa2d524227db716980
MischaGithub <mischa.achmat120212@gmail.com>
2020-06-30 10:26:56 -1000

    just added a background image for now

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/pages/main";
 import { Link } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="demo-big-content">
-        <Layout>
-          <Header title="Mischa Achmat" scroll>
+      <div
+        style={{
+          height: "100vh",
+          background: "cover",
+          attachment: "fixed",
+          position: "center",
+        }}
+      >
+        <Layout
+          style={{
+            background:
+              "url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover",
+          }}
+        >
+          <Header className="header-color" title="My Portfolio" scroll>
             <Navigation>
               <Link to="/projects">Projects</Link>
               <Link to="/testimonals">Testimonals</Link>
