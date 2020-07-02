3a4bddd2220d01207d00e3e2f719b0e07af9dc35
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-02 06:26:24 -1000

    Updates

---
+++ precache-manifest.21f3e25790dc4e9cee53cb6dd58bb1e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682cc10063d77503517d5143c6f2a001",
+    "url": "/My-Portfolio-Project/index.html"
+  },
+  {
+    "revision": "b63b71f260eef161fbc7",
+    "url": "/My-Portfolio-Project/static/css/2.5206c547.chunk.css"
+  },
+  {
+    "revision": "913b6bddd0644530018e",
+    "url": "/My-Portfolio-Project/static/css/main.432bbc16.chunk.css"
+  },
+  {
+    "revision": "b63b71f260eef161fbc7",
+    "url": "/My-Portfolio-Project/static/js/2.4654ecdb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/My-Portfolio-Project/static/js/2.4654ecdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913b6bddd0644530018e",
+    "url": "/My-Portfolio-Project/static/js/main.028129bf.chunk.js"
+  },
+  {
+    "revision": "95e8b4f3952b607ce9fb",
+    "url": "/My-Portfolio-Project/static/js/runtime-main.9daad398.js"
+  }
+]);
