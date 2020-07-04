29d54cd9ee6e7a089ff0a78fd8cd6180baed366e
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-04 09:30:54 -1000

    Updates

---
+++ precache-manifest.2788803b620e7e89868611b508ea1c13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ea3f0378cde7b6e3f30c2df38e75b0",
+    "url": "/My-Portfolio-Project/index.html"
+  },
+  {
+    "revision": "acf21c8a602bb6d32041",
+    "url": "/My-Portfolio-Project/static/css/2.01d06922.chunk.css"
+  },
+  {
+    "revision": "225d340b3d2861cadc9a",
+    "url": "/My-Portfolio-Project/static/css/main.b6c2ff2b.chunk.css"
+  },
+  {
+    "revision": "acf21c8a602bb6d32041",
+    "url": "/My-Portfolio-Project/static/js/2.a44a546e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/My-Portfolio-Project/static/js/2.a44a546e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225d340b3d2861cadc9a",
+    "url": "/My-Portfolio-Project/static/js/main.0a9849e5.chunk.js"
+  },
+  {
+    "revision": "95e8b4f3952b607ce9fb",
+    "url": "/My-Portfolio-Project/static/js/runtime-main.9daad398.js"
+  },
+  {
+    "revision": "4b308db08babc336eeab614f7ee62c77",
+    "url": "/My-Portfolio-Project/static/media/Mischa_about1.4b308db0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/My-Portfolio-Project/precache-manifest.b171aa92d17a7e1b1699424d829395c4.js"
+  "/My-Portfolio-Project/precache-manifest.2788803b620e7e89868611b508ea1c13.js"
 );
 
 self.addEventListener('message', (event) => {
