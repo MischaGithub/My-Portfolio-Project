37c21c342dd389588bf668c0e9373690baad243a
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-05 04:30:24 -1000

    just added some lorem ipsum to check the sizing of the cards

--- src/components/pages/testimonals.js
+++ src/components/pages/testimonals.js
@@ -19,8 +19,19 @@ class Testimonals extends Component {
           >
             Ariya Lucas
           </CardTitle>
-          <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-            Testimonal
+          <CardText
+            style={{
+              fontSize: "1em",
+              textTransform: "uppercase",
+              color: "#111",
+            }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
           </CardText>
         </Card>
 
@@ -37,8 +48,19 @@ class Testimonals extends Component {
           >
             Aaqiel Behardien
           </CardTitle>
-          <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-            Testimonal
+          <CardText
+            style={{
+              fontSize: "1em",
+              textTransform: "uppercase",
+              color: "#111",
+            }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
           </CardText>
         </Card>
 
@@ -54,8 +76,19 @@ class Testimonals extends Component {
           >
             Hishaam De Vries
           </CardTitle>
-          <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-            Testimonal
+          <CardText
+            style={{
+              fontSize: "1em",
+              textTransform: "uppercase",
+              color: "#111",
+            }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
           </CardText>
         </Card>
 
@@ -71,8 +104,19 @@ class Testimonals extends Component {
           >
             Damon Anthony
           </CardTitle>
-          <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-            Testimonal
+          <CardText
+            style={{
+              fontSize: "1em",
+              textTransform: "uppercase",
+              color: "#111",
+            }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
           </CardText>
         </Card>
 
@@ -88,8 +132,19 @@ class Testimonals extends Component {
           >
             Viata Fredricks
           </CardTitle>
-          <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-            Testimonal
+          <CardText
+            style={{
+              fontSize: "1em",
+              textTransform: "uppercase",
+              color: "#111",
+            }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
           </CardText>
         </Card>
       </div>
