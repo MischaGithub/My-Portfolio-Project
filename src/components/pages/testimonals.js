0caede0af7f8a5ffac14c8d3c664f604b64b7717
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-25 12:04:22 -1000

    Changed the testimonal images as per request and sizes

--- src/components/pages/testimonals.js
+++ src/components/pages/testimonals.js
@@ -1,139 +1,96 @@
 import React, { Component } from "react";
 
-import { Card, CardTitle, CardText } from "react-mdl";
+import { Card } from "react-mdl";
+
+import Godwin from "../assets/godwin-dzvapatsra.jpg";
 
 class Testimonals extends Component {
   render() {
     return (
-      <div className="testimonal-grid ">
-        {/* Card 1 Ariya Testimonal*/}
+      <div className="testimonal-container">
+        {/* Card 1 Godwin Testimonal*/}
 
-        <Card shadow={5} className="testimonal-cards" style={{ margin: "2em" }}>
-          <CardTitle
-            style={{
-              color: "#000",
-              height: "380px",
-              background:
-                "url(https://static.vecteezy.com/system/resources/previews/000/148/181/original/testimonials-icon-set-vector.png) center / cover",
-            }}
-          >
+        <Card shadow={5} className="testimonal-cards ">
+          <div>
+            <img src={Godwin} alt="godwin" className="testimonal-icon" />
+          </div>
+          <p style={{ margin: "0.2rem auto", justifyContent: "center" }}>
             Godwin Dzvapatsva
-          </CardTitle>
-          <CardText
-            style={{
-              fontSize: "1.1em",
-              color: "#111",
-            }}
-          >
+          </p>
+          <div className="container">
             Mischa is a strong-minded students and has the courage to stand up
             to challenge constructively. Mischa communicates well when in doubt
             with the mentor and I highly recommend her. Godwin Dzvapatsva (Head
             of Curriculum and Learning)
-          </CardText>
+          </div>
         </Card>
 
         {/* Card 2 Aaqiel Behardien */}
-        <Card shadow={5} className="testimonal-cards" style={{ margin: "2em" }}>
-          <CardTitle
-            style={{
-              color: "#111",
-              height: "380px",
-              background:
-                "url(https://static.vecteezy.com/system/resources/previews/000/148/181/original/testimonials-icon-set-vector.png) center / cover",
-            }}
-          >
+        <Card shadow={5} className="testimonal-cards">
+          <div>
+            <img src={Godwin} alt="godwin" className="testimonal-icon" />
+          </div>
+          <p style={{ margin: "0.2rem auto", justifyContent: "center" }}>
             Aaqiel Behardien
-          </CardTitle>
-          <CardText
-            style={{
-              fontSize: "1.1em",
-              color: "#111",
-            }}
-          >
+          </p>
+          <div className="container">
             If you are looking for a hard-working individual who knows how to
             have fun, then look no further. Mischa is a keen developer and is
             simply outstanding at everything she does and is always ready to
             assist those in need. But beware, she becomes a an unstoppable force
             when chasing her goals. Aaqiel Behardien (Developer and friend)
-          </CardText>
+          </div>
         </Card>
 
-        {/* Card 3 Hishaam De Vries */}
-        <Card shadow={5} className="testimonal-cards" style={{ margin: "2em" }}>
-          <CardTitle
-            style={{
-              color: "#000",
-              height: "380px",
-              background:
-                "url(https://static.vecteezy.com/system/resources/previews/000/148/181/original/testimonials-icon-set-vector.png) center / cover",
-            }}
-          >
+        {/* Card 3 Lukhanyo Vakele */}
+        <Card shadow={5} className="testimonal-cards">
+          <div>
+            <img src={Godwin} alt="godwin" className="testimonal-icon" />
+          </div>
+          <p style={{ margin: "0.2rem auto", justifyContent: "center" }}>
             Lukhanyo Vakele
-          </CardTitle>
-          <CardText
-            style={{
-              fontSize: "1.1em",
-              color: "#111",
-            }}
-          >
+          </p>
+          <div className="container">
             Mischa is an intelligent, hard working human. She is very dedicated
             in always producing the best in her work. She is an excellent
             problem solver who is always aiming to do things right rather than
             fast. She is an easy person to work with and I have seen her display
             a positive attitude towards her work. Lukhanyo Vakele (Peer)
-          </CardText>
+          </div>
         </Card>
 
         {/* Card 4 Damon Anthony */}
-        <Card shadow={5} className="testimonal-cards" style={{ margin: "2em" }}>
-          <CardTitle
-            style={{
-              color: "#000",
-              height: "380px",
-              background:
-                "url(https://static.vecteezy.com/system/resources/previews/000/148/181/original/testimonials-icon-set-vector.png) center / cover",
-            }}
-          >
+        <Card shadow={5} className="testimonal-cards">
+          <div>
+            <img src={Godwin} alt="godwin" className="testimonal-icon" />
+          </div>
+          <p style={{ margin: "0.2rem auto", justifyContent: "center" }}>
             Damon Anthony
-          </CardTitle>
-          <CardText
-            style={{
-              fontSize: "1.1em",
-              color: "#111",
-            }}
-          >
+          </p>
+          <div className="container">
             Mischa is a hardworking and determined individual. She is a
             dedicated problem solver who stays focused on the task at hand. She
             has good communication skills and can work independently as well as
             with others. Mischa will be an asset in the workplace. Damon
             Anthony(Peer)
-          </CardText>
+          </div>
         </Card>
 
         {/* Card 5 Viata Fredricks */}
-        <Card shadow={5} className="testimonal-cards" style={{ margin: "2em" }}>
-          <CardTitle
-            style={{
-              color: "#000",
-              height: "380px",
-              background:
-                "url(https://static.vecteezy.com/system/resources/previews/000/148/181/original/testimonials-icon-set-vector.png) center / cover",
-            }}
-          >
+        <Card shadow={5} className="testimonal-cards">
+          <div>
+            <img src={Godwin} alt="godwin" className="testimonal-icon" />
+          </div>
+          <p style={{ margin: "0.2rem auto", justifyContent: "center" }}>
             Viata Fredricks
-          </CardTitle>
-          <CardText
-            style={{
-              fontSize: "1.1em",
-              color: "#111",
-            }}
-          >
+          </p>
+          <div className="container">
             Mischa is a passionate, assertive and hardworking individual. She's
             always willing to learn and isn't afraid to ask questions. Mischa is
             able to work well with others and puts in great efforts to achieve
             her goals. I believe she will do well in the workplace. Viata
             Fredricks(Peer)
-          </CardText>
+          </div>
         </Card>
       </div>
     );
