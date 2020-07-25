2f0ee7be6d8ee2d01a85f49031121766c032b536
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-25 09:05:33 -1000

    Project Section resized the cards to a small height also changed the icon to a constant icon rather then different ones for each project

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
-import { Card, CardTitle, CardText, CardActions } from "react-mdl";
+import { Card, CardText, CardActions } from "react-mdl";
+
+import Project_Icon from "../assets/project_icons_2.jpg";
 
 class Projects extends Component {
   render() {
     return (
       <div className="projects-body">
-        <div className="projects-grid">
+        <h2 style={{ textAlign: "center", fontWeight: "bolder" }}>
+          My Projects
+        </h2>
+        <div className="projects-container">
           {/* Card 1 React Project #1*/}
-          <Card shadow={4} className="project-cards" style={{ margin: "2em" }}>
-            <CardTitle
-              style={{
-                color: "#000",
-                height: "400px",
-                background:
-                  "url(https://i.pinimg.com/originals/8f/da/4b/8fda4ba42cdc0ce3c6c85832bb076d02.jpg) center / cover",
-              }}
-            >
-              React Project #1
-            </CardTitle>
-            <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-              Github Finder
-            </CardText>
+          <Card shadow={4} className="project-cards">
+            <div>
+              <img
+                src={Project_Icon}
+                alt="projects_icon"
+                className="project-icon"
+              />
+            </div>
+            <p style={{ fontSize: "20px" }}>React Project #1</p>
+
             <CardActions border>
+              <div>
+                <p style={{ fontWeight: "bolder" }}>Github Finder</p>
+                <p>
+                  {" "}
+                  The Github finder gets all the users info from the github api
+                  and display the user in a card format and with the on click of
+                  more you can view a more detailed profile of the user.
+                </p>
+              </div>
               <div className="social-links">
                 {/* Live Demo */}
                 <a
@@ -44,21 +54,25 @@ class Projects extends Component {
           </Card>
 
           {/* Card 2 React Project #2 */}
-          <Card shadow={5} className="project-cards" style={{ margin: "2em" }}>
-            <CardTitle
-              style={{
-                color: "#000",
-                height: "400px",
-                background:
-                  "url(https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-and-shapes-4/177800/201-512.png) center / cover",
-              }}
-            >
-              React Project #2
-            </CardTitle>
-            <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-              Contact Keeper
-            </CardText>
+          <Card shadow={4} className="project-cards">
+            <div>
+              <img
+                src={Project_Icon}
+                alt="projects_icon"
+                className="project-icon"
+              />
+            </div>
+            <p style={{ fontSize: "20px" }}>React Project #2</p>
             <CardActions border>
+              <div>
+                <p style={{ fontWeight: "bolder" }}>Contact Keeper</p>
+                <p>
+                  {" "}
+                  The Contact Keeper was created for keeping track of user
+                  contacts with in a CRUD system. The users can update, add,
+                  delete and edit their contacts.
+                </p>
+              </div>
               <div className="social-links">
                 {/* Live Demo */}
                 <a
@@ -81,21 +95,26 @@ class Projects extends Component {
           </Card>
 
           {/* Card 3 React Project #3 */}
-          <Card shadow={5} className="project-cards" style={{ margin: "2em" }}>
-            <CardTitle
-              style={{
-                color: "#000",
-                height: "400px",
-                background:
-                  "url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0Ljs-_14R0TpjjqmYj7Sqnnmu_Gi19z9K6w&usqp=CAU) center / cover",
-              }}
-            >
-              React Project #3
-            </CardTitle>
-            <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-              IT LOGGER
-            </CardText>
+          <Card shadow={4} className="project-cards">
+            <div>
+              <img
+                src={Project_Icon}
+                alt="projects_icon"
+                className="project-icon"
+              />
+            </div>
+            <p style={{ fontSize: "20px" }}>React Project #3</p>
+
             <CardActions border>
+              <div>
+                <p style={{ fontWeight: "bolder" }}>IT Logger</p>
+                <p>
+                  {" "}
+                  The IT Logger is a system built for logging any and all IT
+                  system problems it. You are able to give a details summary of
+                  what problem is. Also add a Tech to the system
+                </p>
+              </div>
               <div className="social-links">
                 {/* Github */}
                 <a
@@ -110,21 +129,26 @@ class Projects extends Component {
           </Card>
 
           {/* Card 1 Python Project #1*/}
-          <Card shadow={5} className="project-cards" style={{ margin: "2em" }}>
-            <CardTitle
-              style={{
-                color: "#000",
-                height: "400px",
-                background:
-                  "url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSIRtTHoo3u9yOySxuq3_DMoSA0qeAOwEvUzA&usqp=CAU) center / cover",
-              }}
-            >
-              Python Project #1
-            </CardTitle>
-            <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-              Lottery System
-            </CardText>
+          <Card shadow={4} className="project-cards">
+            <div>
+              <img
+                src={Project_Icon}
+                alt="projects_icon"
+                className="project-icon"
+              />
+            </div>
+            <p style={{ fontSize: "20px" }}> Python Project #1</p>
+
             <CardActions border>
+              <div>
+                <p style={{ fontWeight: "bolder" }}>Lottery System</p>
+                <p>
+                  The Lottery System was build to generate random lottery
+                  numbers. Once a user selects their 6 numbers it will match
+                  what the system kicks out and then match the numbers based on
+                  a prize category.
+                </p>
+              </div>
               <div className="social-links">
                 {/* Github */}
                 <a
@@ -139,21 +163,29 @@ class Projects extends Component {
           </Card>
 
           {/* Card 2 Python Project #2*/}
-          <Card shadow={5} className="project-cards" style={{ margin: "2em" }}>
-            <CardTitle
-              style={{
-                color: "#000",
-                height: "400px",
-                background:
-                  "url(https://i7.pngguru.com/preview/517/876/849/purple-symbol-brand-violet-time-machine.jpg) center / cover",
-              }}
-            >
-              Python Project #2
-            </CardTitle>
-            <CardText style={{ fontSize: "20px", textTransform: "uppercase" }}>
-              LifeChoices Attendance Log
-            </CardText>
+          <Card shadow={4} className="project-cards">
+            <div>
+              <img
+                src={Project_Icon}
+                alt="projects_icon"
+                className="project-icon"
+              />
+            </div>
+            <p style={{ fontSize: "20px" }}> Python Project #2</p>
+
             <CardActions border>
+              <div>
+                <p style={{ fontWeight: "bolder" }}>
+                  {" "}
+                  LifeChoices Attendance Log
+                </p>
+                <p>
+                  The Attendance Log for LifeChoices was created for keeping
+                  track of visitors and gives them the option to log in and
+                  register if they are new but also its use is for employees for
+                  daliy Attendance.
+                </p>
+              </div>
               <div className="social-links">
                 {/* Github */}
                 <a
